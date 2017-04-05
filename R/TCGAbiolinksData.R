67fe713dee5dcdc55ceb8fca9816c58218ac285a
tiagochst <tiagochst@gmail.com>
2017-04-05 13:07:19 -0300

    adding new files

---
+++ R/TCGAbiolinksData.R
@@ -0,0 +1,16 @@
+#' @title Data for TCGAbiolinks package 
+#' @description
+#' This package provides all necessary data to run and compile TCGAbiolinks package.
+#' For more information how to create these objects please read the vignette of this package with the
+#' follwing command: \code{browseVignettes("TCGAbiolinksData")}
+#'
+#' @docType package
+#' @name TCGAbiolinksData
+#' @keywords utilities
+#' @examples
+#' data("case1")
+#' data("case2")
+#' data("case3")
+#' data("geneInfoHT")
+#' data("geneInfo")
+NULL
